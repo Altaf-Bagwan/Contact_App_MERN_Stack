e94d7ece2e829db706d9b8cdff81601554687630
Altaf Bagwan <altafbagwan808@gmail.com>
2023-09-01 22:03:23 +0530

    fc

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ server.use(cors());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded());
 
-server.use('/static', express.static(path.join(__dirname, 'static')))
+server.use(express.static(path.join(__dirname, 'build')))
 
 
 server.get('/',(req,res)=>{
